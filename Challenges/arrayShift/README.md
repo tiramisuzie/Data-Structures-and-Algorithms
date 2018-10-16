73214070fe16749a6c4326c310144c29e0fe8076
suzanne su <suhysu@gmail.com>
2018-10-16 10:39:19 -0700

    updated

--- Challenges/arrayShift/README.md
+++ Challenges/arrayShift/README.md
@@ -14,4 +14,4 @@ new value added at the middle index.
 
 ## Solution
 
-![Whiteboard]()
+![alt text]
---
+++ Data-Structures/LinkedLists/LinkedLists/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using LinkedLists.Classes;
+
+namespace LinkedLists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Node node1 = new Node(7);
+
+            Node node2 = new Node("cat");
+
+
+
+            LList myAwesomeList = new LList(node1);
+
+            myAwesomeList.Append(node2);
+
+
+
+            myAwesomeList.Print();
+
+
+        }
+    }
+}

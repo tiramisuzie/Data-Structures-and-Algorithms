b66fcf987b7fd93f66177597d7b680cac7e1bda8
suzanne su <suhysu@gmail.com>
2018-10-23 02:18:39 -0700

    run in main

--- Data-Structures/StackAndQueue/StackAndQueue/Program.cs
+++ Data-Structures/StackAndQueue/StackAndQueue/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using StackAndQueue.Classes;
+using System;
 
 namespace StackAndQueue
 {
@@ -6,7 +7,27 @@ namespace StackAndQueue
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Stack stack = new Stack();
+            stack.Push(new Node(1));
+            stack.Push(new Node(2));
+            stack.Push(new Node(3));
+            Console.WriteLine("Peek: " + stack.Peek().Value);
+            Console.WriteLine("Pop: " + stack.Pop().Value);
+            Console.WriteLine("Peek: " + stack.Peek().Value);
+            Console.WriteLine("Pop: " + stack.Pop().Value);
+            Console.WriteLine("Peek: " + stack.Peek().Value);
+            Console.WriteLine("Pop: " + stack.Pop().Value);
+            Console.WriteLine("*********************************");
+            Queue queue = new Queue();
+            queue.Enqueue(new Node(1));
+            queue.Enqueue(new Node(2));
+            queue.Enqueue(new Node(3));
+            Console.WriteLine("Peek: " + queue.Peek().Value);
+            Console.WriteLine("Dequeue: " + queue.Dequeue().Value);
+            Console.WriteLine("Peek: " + queue.Peek().Value);
+            Console.WriteLine("Dequeue: " + queue.Dequeue().Value);
+            Console.WriteLine("Peek: " + queue.Peek().Value);
+            Console.WriteLine("Dequeue: " + queue.Dequeue().Value);
         }
     }
 }

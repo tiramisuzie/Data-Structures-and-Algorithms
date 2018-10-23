82df12a1a9af5a960ec890057be0cdcdd3f4fa61
suzanne su <suhysu@gmail.com>
2018-10-23 02:13:50 -0700

    add node

--- Data-Structures/StackAndQueue/StackAndQueue/Classes/Node.cs
+++ Data-Structures/StackAndQueue/StackAndQueue/Classes/Node.cs
@@ -4,7 +4,41 @@ using System.Text;
 
 namespace StackAndQueue.Classes
 {
-    class Node
+    public class Node
     {
+        /// <summary>
+
+        /// the value contained in the node
+
+        /// </summary>
+
+        public object Value { get; set; }
+
+        /// <summary>
+
+        /// the pointer to the next node
+
+        /// </summary>
+
+        public Node Next { get; set; }
+        public object Current { get; private set; }
+
+
+
+        /// <summary>
+
+        /// constructor for our nodes
+
+        /// </summary>
+
+        /// <param name="value">the value contained in the node</param>
+
+        public Node(object value)
+
+        {
+
+            Value = value;
+
+        }
     }
 }

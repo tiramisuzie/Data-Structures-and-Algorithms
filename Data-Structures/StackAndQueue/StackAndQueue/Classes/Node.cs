84a4ad5a98a31349eb1cd7562d1223b385d413bb
suzanne su <suhysu@gmail.com>
2018-10-23 02:12:52 -0700

    restructure

--- Data-Structures/StackAndQueue/StackAndQueue/Classes/Node.cs
+++ Data-Structures/StackAndQueue/StackAndQueue/Classes/Node.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace StackAndQueue
+namespace StackAndQueue.Classes
 {
     class Node
     {
--- Data-Structures/StackAndQueue/StackAndQueue/Classes/queue.cs
+++ Data-Structures/StackAndQueue/StackAndQueue/Classes/queue.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace StackAndQueue
+namespace StackAndQueue.Classes
 {
     public class Queue
     {

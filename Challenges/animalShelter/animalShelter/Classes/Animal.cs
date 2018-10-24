0e441b40e7f82174bcbb6d136316b6183ce1965b
suzanne su <suhysu@gmail.com>
2018-10-23 21:39:49 -0700

    create animal, dog and cat class

---
+++ Challenges/animalShelter/animalShelter/Classes/Animal.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace animalShelter.Classes
+{
+    // create animal class
+    public class Animal
+    {
+    }
+}
---
+++ Challenges/animalShelter/animalShelter/Classes/Dog.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace animalShelter.Classes
+{
+    // create dog class
+    public class Dog
+    {
+    }
+}

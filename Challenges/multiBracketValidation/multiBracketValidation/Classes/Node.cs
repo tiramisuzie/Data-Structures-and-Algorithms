5c1b003a8e68d607e5e2f7c2ac8513911f008b77
suzanne su <suhysu@gmail.com>
2018-10-25 00:36:21 -0700

    node, stack classes

---
+++ Challenges/multiBracketValidation/multiBracketValidation/Classes/Node.cs
@@ -0,0 +1,40 @@
+﻿namespace multiBracketValidation.Classes
+{
+    public class Node
+    {
+        /// <summary>
+
+        /// the value contained in the node
+
+        /// </summary>
+
+        public object Value { get; set; }
+
+        /// <summary>
+
+        /// the pointer to the next node
+
+        /// </summary>
+
+        public Node Next { get; set; }
+        public object Current { get; private set; }
+
+
+
+        /// <summary>
+
+        /// constructor for our nodes
+
+        /// </summary>
+
+        /// <param name="value">the value contained in the node</param>
+
+        public Node(object value)
+
+        {
+
+            Value = value;
+
+        }
+    }
+}

8e2c55fc317fdb19125f3ad044e13baba0617d46
suzanne su <suhysu@gmail.com>
2018-10-24 23:20:48 -0700

    init

---
+++ Challenges/multiBracketValidation/multiBracketValidation/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace multiBracketValidation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        static void MultiBracketValidation()
+        {
+
+        }
+    }
+}

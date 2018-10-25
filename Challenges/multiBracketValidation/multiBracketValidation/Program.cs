5035a8cbc4cff197d9fb39f886644082550a39c5
suzanne su <suhysu@gmail.com>
2018-10-25 00:35:49 -0700

    verify logic

--- Challenges/multiBracketValidation/multiBracketValidation/Program.cs
+++ Challenges/multiBracketValidation/multiBracketValidation/Program.cs
@@ -1,17 +1,69 @@
-﻿using System;
+﻿using multiBracketValidation.Classes;
+using System;
 
 namespace multiBracketValidation
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine(MultiBracketValidation("([])"));
         }
 
-        static void MultiBracketValidation()
+        public static bool MultiBracketValidation(string input)
         {
+            Stack stack = new Stack();
+            char[] characters = input.ToCharArray();
+            for(int i = 0; i < characters.Length; i++)
+            {
+                if (ValidateOpenBracket(characters[i]))
+                {
+                    stack.Push(new Node(characters[i]));
+                }
+                else if (ValidateCloseBracket(characters[i]))
+                {
+                    try
+                    {
+                        var openBracket = stack.Pop();
+                    
+                        if (!(ValidateCompleteSquareBracket((char)openBracket.Value, characters[i]) ||
+                            ValidateCompleteCurlyBracket((char)openBracket.Value, characters[i]) ||
+                            ValidateCompleteRoundBracket((char)openBracket.Value, characters[i])))
+                        {
+                            return false;
+                        }
+                    } catch (InvalidOperationException)
+                    {
+                        return false;
+                    }
+                }
+            }
+            return stack.size == 0;
+        }
 
+        public static bool ValidateOpenBracket(char c)
+        {
+            return c == '[' || c == '{' || c == '(';
+        }
+
+        public static bool ValidateCloseBracket(char c)
+        {
+            return c == ')' || c == ']' || c == '}';
+        }
+
+        static bool ValidateCompleteSquareBracket(char openBracket, char closeBracket)
+        {
+            return openBracket == '[' && closeBracket == ']';
+        }
+
+        static bool ValidateCompleteRoundBracket(char openBracket, char closeBracket)
+        {
+            return openBracket == '(' && closeBracket == ')';
+        }
+
+        static bool ValidateCompleteCurlyBracket(char openBracket, char closeBracket)
+        {
+            return openBracket == '{' && closeBracket == '}';
         }
     }
 }
